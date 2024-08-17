91a55591f1d319847ea247f00f08573476de4d73
Ruukaas <lucashenriquehca@gmail.com>
2024-08-17 20:18:56 -0300

    Prática 6 - Parte 3

---
+++ app/src/main/java/com/weatherapp/db/fb/FBCity.kt
@@ -0,0 +1,22 @@
+package com.weatherapp.db.fb
+
+import com.google.android.gms.maps.model.LatLng
+import com.weatherapp.model.City
+
+class FBCity() {
+    var name : String? = null
+    var lat : Double? = null
+    var lng : Double? = null
+
+    fun toCity(): City {
+        val latlng = LatLng(lat ?: 0.0, lng ?: 0.0)
+        return City(name!!, weather = "", location = latlng)
+    }
+}
+fun City.toFBCity() : FBCity {
+    val fbCity = FBCity()
+    fbCity.name = this.name
+    fbCity.lat = this.location?.latitude ?: 0.0
+    fbCity.lng = this.location?.longitude ?: 0.0
+    return fbCity
+}
